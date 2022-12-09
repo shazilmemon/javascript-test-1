f2343b7330bbd0db686f451220d3bad490ab5916
Shazil <shazilmemon6@gmail.com>
2022-12-09 12:20:34 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,479 @@
+// answer no: 1
+// alert("welcome to java script")
+
+// answer no: 2
+//  alert("error! enter a valid password")
+
+// answer no: 3
+// alert("welcome to js land... \n happy coding!");
+
+// answer no : 4
+
+// alert("welcome to js land...")
+// alert("happy coding!  \n prevent this page from creating additional dialog".);
+
+// answer no : 5
+
+// alert("hello... i can run js through web browser console")
+
+// answer no : 6
+
+// chapter one END!
+
+                             // chapter 2 start
+// answer 1,2,3 are mixed
+
+// var a = "username"
+// var b = "shazil ahmed"
+// var message = "hello world"
+//  alert("hello world")
+
+// answer no: 4
+
+// var name = "ahmed";
+// var father  = "abdul";
+// var  age  = "18";
+
+//     alert(name );
+//     alert(father );
+//     alert(age );
+
+// answer no: 5
+
+//
+
+// answer no : 6
+
+// alert("my email address : example123@gmail.com")
+
+// answer no : 7
+
+// alert("i am trying to learn from a book smarter way to learn javascript")
+
+// answer no : 8
+/* <body> 
+
+<p>YAH! i can write HTML content through java script</p>
+
+</body> */
+
+// answer no : 9
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// chapter 2 END
+
+                                       //   chapter 3 start
+// answer no : 10
+
+// alert("iam 18 years old")
+
+// answer no : 11
+
+// var time = "10";
+// var visit = "webpage";
+
+// alert("time");
+// alert("visit");
+// alert("you have visit this site 10 times");
+
+// answer no : 12
+
+// document.write("JOHN DOE ordered 5 T-SHIRT(S) ON XYZ CLOTHING STORE")
+
+
+
+// chapter 3 END
+
+//  chapter 4 start
+
+//    answer no 13
+ 
+
+//  document.write("variable names can only contain,numbers $ and - . for example: $my_1stVariable <br>")
+//  document.write("variables must begin with a letter $ or - . For example $name, _name or name <br>")
+//  document.write("variables names are case sensitive <br>")
+//  document.write("variables names should not be js keywords <br>")
+
+
+
+
+
+
+
+
+//  chapter 5 start
+
+// answer no : 14
+
+// var a = 3;
+// var b = 5;
+// var c = 8;
+// alert d ("18")
+
+// answer no : 15
+
+// var valOne = +prompt("val1");
+// var opT = +prompt('op')
+// var valTwo =+prompt('val 2')
+
+// if(valOne+valTwo){
+// alert(valOne+valTwo)
+// }
+// if(valOne-valTwo){
+//     alert(valOne-valTwo)
+// }
+
+// if(valOne*valTwo){
+//     alert(valOne*valTwo)
+// }
+
+// if(valOne/valTwo){
+//     alert(valOne/valTwo)
+// }
+
+// if(valOne%valTwo){
+//     alert(valOne%valTwo)
+// }
+
+// answer no : 16  not understand
+
+// answer no 17
+
+// var val3 = +prompt(val3)
+// var operator = +prompt(operator)
+// var val4 = +prompt(val4)
+// if(val3*val4){
+
+//     alert("total cost to buy 5 tickets to a movie is 3000 PKR")
+// }
+
+// answer no 17
+//  ("table of 4")
+
+// document.write("4" + "x" + "1" + "=" + 4*1 + "<br>")
+// document.write("4" + "x" + "2" + "=" + 4*2 + "<br>")
+// document.write("4" + "x" + "3" + "=" + 4*3 + "<br>")
+// document.write("4" + "x" + "4" + "=" + 4*4 + "<br>")
+// document.write("4" + "x" + "5" + "=" + 4*5 + "<br>")
+// document.write("4" + "x" + "6" + "=" + 4*6 + "<br>")
+// document.write("4" + "x" + "7" + "=" + 4*7 + "<br>")
+// document.write("4" + "x" + "8" + "=" + 4*8 + "<br>")
+// document.write("4" + "x" + "9" + "=" + 4*9 + "<br>")
+// document.write("4" + "x" + "10" + "=" + 4*10 + "<br>")
+
+// answer no 18 not understand
+
+// answer no 19
+
+// document.write("shopping cart <br>")
+
+// document.write("price of item 1 is 500RS  <br>")
+// document.write("quantity of item 1 is (5) <br>")
+// document.write("price of item 2 is 1000RS <br>")
+// document.write("quantity of item 2 is (10 <br>")
+
+// document.write("total cost of your order is 50,000")
+
+// answer no 20
+
+// document.write   ("marks sheet <br>")
+
+// document.write("total marks: 1100 <br>")
+// document.write("marks obtained: 1000 <br>")
+// document.write("percentage 95% <br>")
+
+// answer no 21
+
+// document.write("currency in PKR <br>")
+
+// document.write("total currency in pkr 1740")
+
+// answer no 22
+
+// var valOne = +prompt("val1");
+// var opT = +prompt('op')
+// var valTwo =+prompt('val 2')
+
+// if(valOne*valTwo){
+//         alert(valOne*valTwo)
+//     }
+
+//     if(valOne/valTwo){
+//         alert(valOne/valTwo)
+//     }
+
+// answer no 23
+
+// document.write("age calculator <br>")
+
+// document.write("current year: 2022 <br>")
+// document.write("birth year 2005 <br>")
+// document.write("your age is 17")
+
+// answer no 24 not understand
+
+// answer no 26
+
+// document.write("favourite snack: fries <br>")
+// document.write("current age is 18 <br>")
+// document.write("estimate maximum age is 80 <br>")
+// document.write("amount of snacks per day 2 <br>")
+// document.write("You will need 160 fries to last you until the ripe old age of 80”")
+
+// chapter 5 END
+
+//  chapter 6 to 9 starts
+
+// answer no 27
+
+// document.write("Results <br>")
+// document.write("the value of a is: 10 <br>")
+// document.write("................................... <br>")
+
+// document.write("the value of ++a is: 11 <br>")
+// document.write("now the value of a is :11 <br>")
+// document.write("................................... <br>")
+
+// document.write("the value of a++ is: 11 <br>")
+// document.write("now the value of a is: 12 <br>")
+// document.write("................................... <br>")
+
+// document.write("the value of --a is: 11 <br>")
+// document.write("now the value of a is: 11 <br>")
+// document.write("................................... <br>")
+
+// document.write("the value of a-- is: 11 <br>")
+// document.write("now the value of a is: 11 ")
+
+// answer no 28
+
+// var a = 2
+// var b = 1
+
+// document.write("a is 2  <br>")
+// document.write("b  is 1 <br>")
+// document.write("result  is 1 <br>")
+
+// answer no 29
+
+// var name = +prompt("enter your name");{
+//     alert("hello gentle man")
+// }
+
+// answer no 30 not under stand
+
+// answer no 32  cancelled
+
+
+
+// chapter 6 to 9 END
+
+// CHAPTER 9 TO 11 STARTS
+//     answer no 33
+
+// var city = +prompt("city");
+// var karachi = +prompt('karachi');
+
+//     alert("welcome to city of lights")
+
+// answer no 34
+
+// var gender=+prompt("enter your gender")
+
+// if(gender === male){
+// alert("good morning sir")
+// }
+
+// else if (gender === female){
+
+//     alert("good morning mam")
+// }
+// else{
+
+//     alert("tiktoker")
+// }
+
+// answer no 35 not understand
+
+// document.write("signal colour")
+// document.write    ("message")
+
+// answer no 36
+
+// var remainingfuel=+prompt("enter your  fuel")
+// var  currentfuel=+prompt("your remaining fuel is less than 0.25 litres")
+
+//     alert("please refill your fuel")
+
+// answer no 37
+
+//  var a = 4;
+// if(a++===5);
+
+//     alert("given condition for variable a is true")
+
+//      var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False")
+// }
+
+// answer no 38 to be continued
+
+// var secretnumber=+prompt("enter your secret number")
+
+// var a = 1
+// var b = 2
+// var c = 3
+// var d = 4
+// var e = 5
+// var f = 6
+// var g = 7
+// var h = 8
+// var i = 9
+// var j = 10
+
+// if(5==5){
+
+//     alert("bingo! correct answer")
+// }
+
+// if(a,b,c,d,f,g,h,i,j){
+
+//     alert("close enough to correct answer")
+// }
+
+// answer no 39
+
+// var temperature =+prompt("enter the city temperature")
+
+// var a =  40
+// var b =  30
+// var c =  20
+// var d =  10
+
+// if (a == 40){
+//     alert("It is too hot outside.")
+// }
+
+// var temperature =+prompt("enter the city temperature")
+
+// if (b == 30){
+//     alert("the wheather today is normal.")
+// }
+
+// var temperature =+prompt("enter the city temperature")
+
+// if (c == 20){
+//     alert("today wheather is cool.")
+// }
+
+// var temperature =+prompt("enter the city temperature")
+
+// if (d == 10){
+//     alert("OMG today wheather is so cool.")
+// }
+
+// answer no 40
+
+// var val1= prompt("enter your first value")
+// var val2= prompt("enter your second value")
+// var sign= prompt("enter your operator")
+
+// console.log(val1+sign+val2)
+// if(sign === '+'){
+
+//     alert(val1+sign+val2)
+// }else if(sign === '-'){
+
+//     alert(val1-val2)
+// }else if(sign === '*'){
+
+//     alert(val1*val2)
+// }else if(sign === '/'){
+
+//     alert(val1/val2)
+// }
+
+// else if(sign === '%'){
+
+//     alert(val1%val2)
+// }
+
+// chapter 6 to 9 end
+
+// chapter 10 to 11 start
+
+// answer no 41 not understand
+
+// answer no 42
+// var text = prompt("enter value");
+// var aschi = text.charCodeAt(0);
+// console.log("check", aschi);
+// if (aschi >= 65 && aschi <= 90) {
+//     console.log("capital");
+// } else {
+//     console.log("small");
+// }
+
+
+// answer no 43
+
+
+// var a = +prompt("enter your password");
+// var b = + prompt("re type your password");{
+
+//     alert("“Correct! The password you entered matches the original password")
+// }
+
+
+
+// answer no 44 and 45 not understand
+
+
+// var hour = 13
+// if(hour < 18){
+
+//     alert("good evening")
+// }
+
+
+
+// var text = prompt("enter your value");
+// var ashci = text.charCodeAt(0);
+// console.log("check", ashci);
+// if (ashci >= 65 && ashci <= 91){
+// console.log("capital");
+
+// }else {
+// console.log("small");
+// }
+
+
+
+
